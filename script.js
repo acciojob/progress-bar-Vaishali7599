620a9585fc6e8cbfda2ffdd5c7e80dece0199857
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-09-25 01:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll(".circle")
+const lines = document.querySelectorAll(".line")
+const next = document.querySelector("#next")
+const prev = document.querySelector("#prev")
